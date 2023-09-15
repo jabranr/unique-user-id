e6d266e50a671fec22e19b04155e09ca2f2963c6
Jabran Rafique <jabran.rafique@ratedpeople.com>
2023-09-15 10:44:49 +0100

    update tests

---
+++ README.md
@@ -0,0 +1,31 @@
+# Unique User ID
+
+Generate unique id using timestamp and random number.
+
+## Usage
+
+Install package
+
+```sh
+npm install unique-user-id
+```
+
+Use it generate an ID
+
+```js
+import uniqueUserId from 'unique-user-id';
+```
+
+## Run tests
+
+Requires node v20 test runner.
+
+```
+npm t
+```
+
+## License
+
+MIT License
+
+&copy; 2023 - Jabran Rafique
--- test.js
+++ test.js
@@ -1,28 +1,32 @@
-import test from 'node:test';
+import { describe, test } from 'node:test';
 import assert from 'node:assert';
 
 import UniqueId from './index.js';
 
-test('generates a unique ID', (t) => {
-  assert.strictEqual(Boolean(UniqueId()), true);
-});
-
-test('generated ID is alphanumric', (t) => {
-  assert.strictEqual(/^[A-z0-9]+$/.test(UniqueId()), true);
-
-  const uid = UniqueId() + '£$%^&*';
-  assert.strictEqual(/^[A-z0-9]+$/.test(uid), false);
-});
-
-const ids = [];
-for (let i = 1; i <= 100; i++) {
-  const uid = UniqueId();
-  test(`generated ID ${i}: ${uid} is unique`, (t) => {
-    if (ids.includes(uid)) {
-      assert.fail('ID is not unique');
-    } else {
-      ids.push(uid);
-      assert.ok(uid, 'ID is not unique');
-    }
+describe('Create unique user ID', () => {
+  test('generates a unique ID', (t) => {
+    assert.strictEqual(Boolean(UniqueId()), true);
   });
-}
+
+  test('generated ID is alphanumric', (t) => {
+    assert.strictEqual(/^[A-z0-9]+$/.test(UniqueId()), true);
+
+    const uid = UniqueId() + '£$%^&*';
+    assert.strictEqual(/^[A-z0-9]+$/.test(uid), false);
+  });
+});
+
+describe('Test unique value with 100 IDs', () => {
+  const ids = [];
+  for (let i = 1; i <= 100; i++) {
+    const uid = UniqueId();
+    test(`generated ID ${i}: ${uid} is unique`, (t) => {
+      if (ids.includes(uid)) {
+        assert.fail('ID is not unique');
+      } else {
+        ids.push(uid);
+        assert.ok(uid, 'ID is not unique');
+      }
+    });
+  }
+});
